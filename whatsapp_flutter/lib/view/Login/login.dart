0ed3e5d57824e1705a675a5b0fc93d13ba12c550
Alcides Wenner <alcideswenner.jus@gmail.com>
2021-09-07 16:08:43 -0300

    Configure Firebase

---
+++ whatsapp_flutter/lib/view/Login/login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text("Olá, Mundo")
+                ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
